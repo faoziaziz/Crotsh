83b8bec661448e6514da331f4b0d57d4fb909ae1
Aziz Amerul Faozi <admin@labseni.com>
2019-11-20 14:34:04 +0700

    Update PM.sh

--- PM.sh
+++ PM.sh
@@ -4,13 +4,13 @@
 
 date
 
-var=$(ps axu | grep -v grep | grep "PrintEmulator.0.13.3 127.0.0.1 trumon 3306 root 'trumon1234!@#$'")
+var=$(ps axu | grep -v grep | grep "PrintEmulator.0.13.3 127.0.0.1 Trumon 3306 trumon 'S@m@st4!@#456'")
         if [ ! -z "$var" ];
                 then echo "found";
         else
           while true
           do
-		./PrintEmulator.0.13.3 127.0.0.1 trumon 3306 root 'trumon1234!@#$'|more
+		./PrintEmulator.0.13.3 127.0.0.1 Trumon 3306 trumon 'S@m@st4!@#456'|more
           done
 fi
 
